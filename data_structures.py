4f7430aed2016a1e794759310c18a3b505482e24
suhel <thecomputersk@gamil.com>
2025-04-10 00:34:58 +0530

    py

---
+++ data_structures.py
@@ -0,0 +1,26 @@
+#Data structure list* 
+
+#1. List   
+# Why need => this is needs for orginize data to retrive it easily.
+
+requrement = ["milk", "eggs", "bread", "butter"]
+#print(requrement)
+ # second item in the list
+requrement.append("cheese") # add item to the list
+#print(requrement)
+
+#print(dir(requrement)) # show all the methods of the list
+
+#Dictionary    Dictionary is a collection of key-value pairs./Distionary is a mutable, unordered collection of items.
+# Why need => this is needs for orginize data to retrive it easily.
+#Distionary used the device information.
+
+Divice_Info = {
+    "brand": "Apple",
+    "model": "iPhone 13",
+    "year": 2021,
+    "color": "black"
+}
+#print(Divice_Info)
+print(Divice_Info["brand"]) # access the value of the key "brand"
+# what are the dictionary methods
