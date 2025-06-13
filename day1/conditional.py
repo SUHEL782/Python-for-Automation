d72727a20d40f561532ef2f033ade5859b1f1e2c
suhel <thecomputersk@gamil.com>
2025-06-13 18:46:36 +0530

    done!

---
+++ day1/conditional.py
@@ -0,0 +1,22 @@
+# conditional statements
+def check_number(num):
+    if num > 0:
+        print("The number is positive.")
+    elif num < 0:
+        print("The number is negative.")
+    else:
+        print("The number is zero.")
+
+# check the which number is odd and even
+num = 10
+def check_even_odd(num):
+    if num % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+# env = "dev"
+# if env == "dev":
+#     print("Development environment")
+# elif env == "prod":
+#     print("Production environment")
---
+++ day1/hello.py
@@ -0,0 +1,42 @@
+print ("Hello suhel khan how are  you doing today")
+
+print(1+2)
+
+
+#multiline comment
+""" This is a multiline comment
+This is the second line of the comment"""
+
+name = "Suhel Khan"
+print("my nae is:" ,  name)
+
+
+#type casting 
+a = 5
+print(float(a))
+
+
+a = 10
+b = 6
+print(a and b)
+
+#convet to decimal to binary
+a = 10
+print(bin(a))  # Output: 0b1010
+#convert to binary to decimal
+a = 0b1010
+print(int(a))  # Output: 10
+a = 6
+print(bin(a))
+
+a = 10.5
+print(int(a))  # Output: 10
+
+#take a input output
+name = input("Enter your name: ")
+print("Hello", name)
+
+# Take two inputs and print their sum
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("The sum is:", num1 + num2)
