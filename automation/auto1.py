6f6c92160c4e848c4e2dae9bca3d3822cfed65a2
suhel <thecomputersk@gamil.com>
2025-06-16 23:12:39 +0530

    Started

--- automation/auto1.py
+++
@@ -1,7 +0,0 @@
-a = 7
-b = 3
-
-if a > b:
-    print(a)
-else:
-    print(b)
--- automation/auto5.py
+++
@@ -1,7 +0,0 @@
-a = [1, 2, 3, 4, 5]
-
-# Reassign 'a' to a new empty list
-a = []
-
-# Print the current state of 'a'
-print("List after reassignment:", a)
